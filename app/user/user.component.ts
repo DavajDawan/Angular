698d1c8707562648348954480aa53ab97567c934
SabooriJavad <javadsaboori32@gmail.com>
2026-02-12 13:26:42 +0100

    Use signal on the user file

--- app/user/user.component.ts
+++ app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,computed,signal } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 //to add dynamic users I use Random choice
@@ -12,16 +12,23 @@ const randomIndex= Math.floor(Math.random()*DUMMY_USERS.length)
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+
+/*   Zone.js
+  selectedUser = DUMMY_USERS[randomIndex]; */
+
+  //signal
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
 
   // Using get built in function in angular to manage
   // dynamic changes ub every render  we do in angular
-  get imagePath() {
+  /* get imagePath() {
     return 'assets/users/'+ this.selectedUser.avatar
-  }
+  } */
+  
+  imagePath = computed(() => ' assets/users/' + this.selectedUser().avatar);
   onSelectUser() {
     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser = DUMMY_USERS[randomIndex];
-    console.log('New SelectorUser ' + this.selectedUser.id);
+    this.selectedUser.set( DUMMY_USERS[randomIndex]);
+    console.log('New SelectorUser ' + this.selectedUser().id);
   }
 }
