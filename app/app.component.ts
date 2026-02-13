fa07693e2b6ddb6fabfc92eb8671c77eec8c179d
SabooriJavad <javadsaboori32@gmail.com>
2026-02-13 11:47:11 +0100

    Add tasks

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,18 +2,31 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent,UserComponent ],
+  imports: [HeaderComponent,UserComponent,TasksComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  //we shall get user id once clicked in useCopmonent
+  //Search in our Db for the user
+  // Send the name of the user to tasksComponent
+
+  /////init value for our selected user
+  selectedUserId = 'u1';
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId)!;
+  }
   
   onSelectUser(id: string) {
+    this.selectedUserId = id;
+
     console.log('Selected user Id ' + id);
     
   }
